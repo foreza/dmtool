481cf5a5a0ba22bc26143bfedcff3fde5acf7f81
Jason Chiu <jforeza@gmail.com>
2018-08-17 08:36:46 -0700

    Add additional function stubs, re-use image from characters object, movement for characters is supported

--- public/javascripts/MapHelper.js
+++ public/javascripts/MapHelper.js
@@ -2,22 +2,22 @@
 
 var map;
 var mapContainer;
+var characterIDArr = [];
 
+var encounterMap = {
 
-var encounterMap =  {
-
-    characters : [],
-    currentlySelected : ""
+    characters: [],
+    currentlySelected: ""
 
 }
 
 
 // On DOM ready
-$(function() {
-    console.log( "MapHelper ready!" );
+$(function () {
+    console.log("MapHelper ready!");
+
     map = $("#map");
     mapContainer = $("#map-container");
-
 
     drawGrid();
 
@@ -35,106 +35,111 @@ function drawGrid() {
     var sizeY = 400;
     var id = 0;
 
-    for (var i = 0; i < sizeX; i+=unitSize ){
-        for (var j = 0; j < sizeY; j+=unitSize ){
-                console.log("Drawing: " + i + ", " + j);
-                map.append(util_generateUnitDom(i,j, id ));
-                ++id;
-            }
+    for (var i = 0; i < sizeX; i += unitSize) {
+        for (var j = 0; j < sizeY; j += unitSize) {
+            console.log("Drawing: " + i + ", " + j);
+            map.append(util_generateUnitDom(i, j, id));
+            ++id;
         }
+    }
 
-        // TODO: IMPLEMENT
+    // TODO: Initialize characters
+   //  $("#0-unit").append(util_generateCharacterDom("over90000"));     // generates but is not yet attached
 
 
-        // Bind on click events
-       
 
-        $(".mapUnit").hover(
-            
-            function (event) {
-                if ($( "#"+event.target.id).children().length >= 1) {
-                    $( "#"+event.target.id ).addClass( "highlight-grid" );
+    // Bind all map units with select / hover logic
+    $(".mapUnit").hover(
 
-                    $("#"+event.target.id).click(function (event) {    
-        
-                        if (encounterMap.currentlySelected != "" && $( "#"+event.target.id).children().length >= 1){       
+        function (event) {
+            if ($("#" + event.target.id).children().length >= 1) {
+                $("#" + event.target.id).addClass("highlight-grid");
 
-                            console.log("Curr select: ", encounterMap.currentlySelected);
-                            console.log("child.." + $( "#"+event.target.id).children().length);
+                $("#" + event.target.id).click(function (event) {
 
-                            moveCharacterPiece(encounterMap.currentlySelected, event.target.id);
-                            console.log("Moved to map unit:", event.target);
-                            $(  "#"+event.target.id  ).removeClass( "highlight-grid" )
+                    if (encounterMap.currentlySelected != "" && $("#" + event.target.id).children().length >= 1) {
+                        moveCharacterPiece(encounterMap.currentlySelected, event.target.id);
+                        console.log("Moved to map unit:", event.target);
+                        $("#" + event.target.id).removeClass("highlight-grid");
 
-                        } else {
+                    } else {
                         console.log("Clicked on map unit:", event.target);
                     }
                 });
-            }        
+            }
         }, function (event) {
-                $(  "#"+event.target.id  ).removeClass( "highlight-grid" )
-    
-            })
+            $("#" + event.target.id).removeClass("highlight-grid");
+        })
 
-        
 
-        // Bind on click events
-       
+}
 
-    $("#0-unit").append(util_generateCharacterDom("over90000"));     // generates but is not yet attached
+// This function detaches something from the dom and moves it to the new location
+function moveCharacterPiece(uID, newLocID) {
+
+    console.log("Moving: " + uID + " to: " + newLocID);
+    var element = $('#' + uID).detach();
+    $("#" + newLocID).append(element);
+    $("#" + newLocID).removeClass("highlight-grid")
+    encounterMap.currentlySelected = "";
+
+
+}
+
+// This will query the DB and get the object from the room collection
+function retrieveEncounterInformation() {
+
+}
+
+function updateEncounterInformation() {
+
+
+}
+
+// If the DB does not yet have information, we should create an object and persist that under the room collection
+function generateEncounterInformation() {
+
+}
+
+
+function drawCharacters(){
+    for (var i = 0; i < characters.length; ++i){
+        $("#" + i + "-unit").append(util_generateCharacterDom(characters[i].id));     // generates but is not yet attached
+    }
 
     $(".charUnit").click(function (event) {
         console.log("Clicked on sprite:", event.target.id);
         encounterMap.currentlySelected = event.target.id;
     });
 
-    
 }
-
-// This function detaches something from the dom and moves it to the new location
-function moveCharacterPiece(uID, newLocID){
-
-        console.log("Moving: " + uID + " to: " + newLocID);
-        var element = $('#'+uID).detach();
-        $("#"+newLocID).append(element);
-        $(  "#"+newLocID).removeClass( "highlight-grid" )
-        encounterMap.currentlySelected = "";
-
-    
-}
-
-// This will query the DB and get the object from the room collection
-function retrieveEncounterInformation(){
-    
-}
-
-// If the DB does not yet have information, we should create an object and persist that under the room collection
-function generateEncounterInformation(){
-
-}
-
-
-
 
 
 // This utility function generates a dom element in HTML to be inserted into the parent dom
-function util_generateUnitDom(x, y, id){
-    var e = "<div id='" + id + "-unit" + "' class='mapUnit' style='left: " + x 
-    + "px; top: " + y + "px;'><span class='locX'>" + x + 
-    "</span><span class='locY'>" + y + 
-    " </span></div>";
+function util_generateUnitDom(x, y, id) {
+    var e = "<div id='" + id + "-unit" + "' class='mapUnit' style='left: " + x
+        + "px; top: " + y + "px;'><span class='locX'>" + x +
+        "</span><span class='locY'>" + y +
+        " </span></div>";
     return e;
 }
 
 // This utility function generates a character in this location. This should be run on init
 function util_generateCharacterDom(uID) {
 
+    var imgSrc = "";
 
-    var e = "<img class='charUnit' id='" + uID + "-mapSprite" + "' src='https://static.giantbomb.com/uploads/original/10/103881/2157564-poring.jpg' height='20px' width='20px'/>";
-    
-    console.log("util_generateCharacterDom", e);
+    try {
+        imgSrc = $('.character[data-fs-id="' + uID + '"]').children('img')[0].src;
+    } catch {
+        console.log("No image attached to char")
+    }    
 
+    if (imgSrc == "" || imgSrc == undefined) {
+        imgSrc= "https://static.giantbomb.com/uploads/original/10/103881/2157564-poring.jpg";
+    }
 
+    var e = "<img class='charUnit' id='" + uID + "-mapSprite" + "' src='" + imgSrc + "' height='20px' width='20px'/>";
     return e;
 }
 
